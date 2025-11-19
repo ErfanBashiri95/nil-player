d5cfe763f849ef7c95ce6bcade6c6fa8c9db1307
ErfanBashiri95 <erfanbashiri@ymail.com>
2025-11-20 00:02:46 +0330

    finall

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -3,4 +3,11 @@ import { createClient } from '@supabase/supabase-js';
 const url = import.meta.env.VITE_SUPABASE_URL;
 const anon = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
+// برای دیباگ
+console.log("SUPABASE URL:", url);
+console.log("SUPABASE ANON:", anon ? "***" : "undefined");
+
+if (!url) throw new Error("supabaseUrl is required");
+if (!anon) throw new Error("supabaseAnonKey is required");
+
 export const supabase = createClient(url, anon);
